c7bc0cc37d27c6b284efde1eacd723badb8f2e96
samsathecoder <alfreidsamsa@gmail.com>
2025-04-22 23:44:53 +0300

    googleanalytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -71,11 +71,29 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="tr">
-      <head><script src="https://analytics.ahrefs.com/analytics.js" data-key="5++2hjj3KkIgS9hHlB7RPA" async></script></head>
+      <head>
+
+      <Script id="gtm-head" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-NH95Z7XM');
+          `}
+        </Script>
+        <script src="https://analytics.ahrefs.com/analytics.js" data-key="5++2hjj3KkIgS9hHlB7RPA" async></script></head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        
+        <noscript>
+          <iframe 
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NH95Z7XM"
+            height="0" 
+            width="0" 
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
                 <Navbar />
     {/* 👇 JSON-LD buraya */}
     <Script id="ld-json" type="application/ld+json">
