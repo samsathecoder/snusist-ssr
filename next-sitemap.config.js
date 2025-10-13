2a50433f2e10d23d30ad65e093af6b6d7cdd7fc1
samsathecoder <alfreidsamsa@gmail.com>
2025-10-14 01:28:03 +0300

    edit for sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,18 @@
+const connectDB = require('./src/lib/mongoose').default;
+const Product = require('./src/models/Product').default;
+
 module.exports = {
   siteUrl: 'https://snusist.com',
   generateRobotsTxt: true,
-  changefreq: 'weekly',
-  priority: 0.7,
-  includePaths: ['/categories', '/blog', '/products'], 
+  sitemapSize: 5000,
+  async additionalPaths(config) {
+    await connectDB();
+    const products = await Product.find().select('slug').lean();
+    return products.map(p => ({
+      loc: `/products/${p.slug}`, // sitemap URL
+      lastmod: new Date().toISOString(),
+      changefreq: 'weekly',
+      priority: 0.8,
+    }));
+  },
 };
